711005158824d5aaea4d1da8ff39e9707e79ac1f
Oliver-08-ojla <oliver2018es@gmail.com>
2025-07-22 11:04:52 -0500

    Diseño de landing page

--- src/app/pages/public/landing/landing.component.ts
+++ src/app/pages/public/landing/landing.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrls: ['./landing.component.css']  // corregí de styleUrl a styleUrls (plural)
 })
 export class LandingComponent {
+  constructor(private router: Router) {}
 
+  goToAuth() {
+    this.router.navigate(['/auth']);
+  }
 }
