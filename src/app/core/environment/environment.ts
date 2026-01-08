2bfb4356a04b96d0cdbf7faf072fc9b4102fa4e8
Chala <filyp007lfcm117@gmail.com>
2026-01-07 21:07:57 -0500

    producion v1

--- src/app/core/environment/environment.ts
+++ src/app/core/environment/environment.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8000/api'
+// };
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000/api'
+  production: true,
+  apiUrl: 'https://tesis-backend-d240.onrender.com'
 };
