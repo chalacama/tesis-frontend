061dd65dee0877df872cd80c57c44b63ec8aa095
Chala <filyp007lfcm117@gmail.com>
2026-04-06 19:19:49 -0500

    save comptoas

---
+++ src/app/shared/UI/components/data/upload-widget/upload-widget.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-widget',
+  imports: [],
+  templateUrl: './upload-widget.component.html',
+  styleUrl: './upload-widget.component.css'
+})
+export class UploadWidgetComponent {
+
+}
